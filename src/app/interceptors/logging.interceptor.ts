1e8e8b2d2eab18be0d14649e4ca8afa653c05bc1
Shyam Pratap <singhshyampratap900@gmail.com>
2026-02-09 23:26:51 +0530

    removed logging

--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -2,15 +2,15 @@ import { HttpEventType, HttpHandlerFn, HttpRequest } from "@angular/common/http"
 import { tap } from "rxjs";
 
 export function loggingInterceptor(r: HttpRequest<unknown>,next: HttpHandlerFn ){
-  console.log('outgoing request');
-  console.log(r)
-  return next(r).pipe(tap({
-    next: event => {
-      if (event.type === HttpEventType.Response){
-        console.log('Incoming response')
-        console.log(event.status)
-        console.log(event.body)
-      }
-    }
-  }))
+  // console.log('outgoing request');
+  // console.log(r)
+  // return next(r).pipe(tap({
+  //   next: event => {
+  //     if (event.type === HttpEventType.Response){
+  //       console.log('Incoming response')
+  //       console.log(event.status)
+  //       console.log(event.body)
+  //     }
+  //   }
+  // }))
 }
